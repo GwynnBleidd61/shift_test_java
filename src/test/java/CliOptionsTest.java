09288fb2b773a59b65226568ddf8bda7809db2cb
zSlayer <albakov.adam2015@gmail.com>
2026-01-20 20:35:38 +0300

    Add CLI flag tests: -a -s -f

--- src/test/java/CliOptionsTest.java
+++ src/test/java/CliOptionsTest.java
@@ -7,11 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CliOptionsTest {
 
-    @Test
-    void no_options_only_files() {
-        CliOptions opt = CliOptions.parse(new String[]{"in1.txt"});
+//    @Test
+//    void no_options_only_files() {
+//        CliOptions opt = CliOptions.parse(new String[]{"in1.txt"});
+//
+//        assertEquals(1, opt.getInputFiles().size());
+//        assertEquals("in1.txt", opt.getInputFiles().getFirst());
+//    }
 
+    @Test
+    void append_flag_sets_append_true() {
+        CliOptions opt = CliOptions.parse(new String[]{"-a", "in1.txt"});
+        assertTrue(opt.isAppend());
         assertEquals(1, opt.getInputFiles().size());
-        assertEquals("in1.txt", opt.getInputFiles().getFirst());
     }
+
+    @Test
+    void no_append_flag_defaults_to_false() {
+        CliOptions opt = CliOptions.parse(new String[]{"in1.txt"});
+        assertFalse(opt.isAppend());
+    }
+
+    @Test
+    void short_stats_flag_sets_stats_mode_short() {
+        CliOptions opt = CliOptions.parse(new String[]{"-s", "in1.txt"});
+        assertEquals(CliOptions.StatsMode.SHORT, opt.getStatsMode());
+    }
+
+    @Test
+    void full_stats_flag_sets_stats_mode_full() {
+        CliOptions opt = CliOptions.parse(new String[]{"-f", "in1.txt"});
+        assertEquals(CliOptions.StatsMode.FULL, opt.getStatsMode());
+    }
+
+    @Test
+    void stats_flags_last_one_wins() {
+        CliOptions opt = CliOptions.parse(new String[]{"-s", "-f", "in1.txt"});
+        assertEquals(CliOptions.StatsMode.FULL, opt.getStatsMode());
+
+        CliOptions opt2 = CliOptions.parse(new String[]{"-f", "-s", "in1.txt"});
+        assertEquals(CliOptions.StatsMode.SHORT, opt2.getStatsMode());
+    }
+
 }
