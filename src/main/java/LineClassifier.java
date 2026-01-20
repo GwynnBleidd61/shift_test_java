1a11630ed1ee864570d5f5e95ad3d0dbd4f99a85
zSlayer <albakov.adam2015@gmail.com>
2026-01-20 21:45:00 +0300

    Implement line classifier

--- src/main/java/LineClassifier.java
+++ src/main/java/LineClassifier.java
@@ -1,6 +1,34 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class LineClassifier {
     public LineClassifier() {
     }
 
-    public static ValueType classify(String line) {}
+    public static ValueType classify(String line) {
+        if (line == null) {
+            return ValueType.STRING;
+        }
+
+        String s = line.trim();
+
+        if (s.isEmpty()) {
+            return ValueType.STRING;
+        }
+
+        try {
+            new BigInteger(s);
+            return ValueType.INTEGER;
+        } catch (NumberFormatException ignored) {
+        }
+
+        try {
+            new BigDecimal(s);
+            return ValueType.FLOAT;
+        } catch (NumberFormatException ignored) {
+
+        }
+        return ValueType.STRING;
+    }
+
 }
