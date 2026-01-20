a1b3578d98249968f885c7fe40d94bc94f0a1a02
zSlayer <albakov.adam2015@gmail.com>
2026-01-20 22:06:47 +0300

    Implement stringstats

--- src/main/java/StringStats.java
+++ src/main/java/StringStats.java
@@ -3,7 +3,21 @@ public class StringStats {
     private Integer minLength = null;
     private Integer maxLength = null;
 
-    public void accept(String s) {}
+    public void accept(String s) {
+        if (s == null) return;
+
+        int len = s.length();
+
+        if (count == 0) {
+            minLength = len;
+            maxLength = len;
+        } else {
+            if (len < minLength) minLength = len;
+            if (len > maxLength) maxLength = len;
+        }
+
+        count ++;
+    }
 
     public long getCount() {
         return count;
